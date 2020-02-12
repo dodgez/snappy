fe4a531f9f2cb2f807bdc4d5158136cce45e9408
dodgez <zacharysdodge@gmail.com>
2020-02-11 23:42:49 -0800

    Add initial file staging code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{App, Arg, AppSettings, SubCommand};
+use std::path::Path;
 
 mod repo;
+mod stage;
 
 fn main() {
     let matches = App::new("Snappy")
@@ -13,9 +15,18 @@ fn main() {
             .arg(Arg::with_name("force")
                 .help("Overwrites an existing repository")
                 .short("f")))
+        .subcommand(SubCommand::with_name("add")
+            .about("Adds an object to the staging area")
+            .arg(Arg::with_name("object_to_stage")
+                .help("The object to add to staging")
+                .required(true)))
         .get_matches();
     
     if let Some(matches) = matches.subcommand_matches("init") {
         repo::init(matches.is_present("force"));
+    }
+
+    if let Some(matches) = matches.subcommand_matches("add") {
+        stage::stage(&Path::new(matches.value_of("object_to_stage").unwrap()));
     }
 }
