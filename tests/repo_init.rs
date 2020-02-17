40b594ba5f6cbd391f2aa4debad06339e00c45d5
Zachary Dodge <zacharysdodge@gmail.com>
2020-02-16 20:31:27 -0800

    Fix old extern crate usage

--- tests/repo_init.rs
+++ tests/repo_init.rs
@@ -1,11 +1,10 @@
-extern crate snappy;
-
+use snappy::repo::init;
 use std::path::Path;
 
 #[test]
 fn test_repo_init() {
     let snap_dir = Path::new(".snappy");
-    snappy::repo::init(true);
+    init(true);
 
     if !snap_dir.exists() {
       panic!("fatal: could not create snappy repository");
